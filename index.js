d1bd5d766ef6b2f36a9ffeaf40ef60608e93ea10
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-07-29 15:03:02 +1000

    Account creation functions in place

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const UsersRepository = require('./repositories/users')
 
-const app = express()
-app.use(bodyParser.urlencoded({extended: true}))
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordConfirmation" placeholder="password confirmation" />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `);
+});
 
-res.send(`<div>
-<form method="POST">
-<input placeholder="Email" name="email"/>
-<input placeholder="Password" name="password"/>
-<input placeholder="Password Confirmation" name="passwordConfirmation"/>
-<button>Sign Up</button>
-</form>
-</div>`)
-})
+app.post('/', async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+const existingUser = await UsersRepository.getOneBy({ email });
+if (existingUser) {
+   return  res.send("User already exists with this email") 
+}
+
+if(password !== passwordConfirmation) {
+    return res.send("Password must match confirmation")
+}
 
 
+  res.send('Account created!!!');
+});
 
-
-
-app.post('/', (req, res) => {
-    console.log(req.body)
-    res.send("Account Created")})
-
-
-app.listen(3000,()=>
-{console.log("Listening")})
+app.listen(3000, () => {
+  console.log('Listening');
+});
--- repositories/users.js
+++ repositories/users.js
@@ -1,51 +1,88 @@
-const fs = require('fs')
+const fs = require('fs');
+const crypto = require('crypto');
 
 class UsersRepository {
-constructor(fileName) {
-if (!fileName) {
-throw new Error ('Creating a repository requires a file name')
-}
-this.fileName = fileName
-try {
-fs.accessSync(this.fileName)
-}
+  constructor(filename) {
+    if (!filename) {
+      throw new Error('Creating a repository requires a filename');
+    }
 
-catch(err) {
-    fs.writeFileSync(this.fileName, '[]')
-
-}
-}
-
-
-async getAll() {
-    // Open the file called this.filename
-    return  JSON.parse(await fs.promises.readFile(this.fileName, {
-      encoding: 'utf8'
-    }));
+    this.filename = filename;
+    try {
+      fs.accessSync(this.filename);
+    } catch (err) {
+      fs.writeFileSync(this.filename, '[]');
+    }
   }
 
-  async create(attributes) {
-const records = await this.getAll();
-records.push(attributes)
+  async getAll() {
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: 'utf8'
+      })
+    );
+  }
 
-await this.writeAll(records)
+  async create(attrs) {
+    attrs.id = this.randomId();
 
+    const records = await this.getAll();
+    records.push(attrs);
+
+    await this.writeAll(records);
   }
 
   async writeAll(records) {
-    await fs.promises.writeFile(this.fileName, JSON.stringify(records, null, 2), {encoding: 'utf8'} )
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(records, null, 2)
+    );
+  }
+
+  randomId() {
+    return crypto.randomBytes(4).toString('hex');
+  }
+
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find(record => record.id === id);
+  }
+
+  async delete(id) {
+    const records = await this.getAll();
+    const filteredRecords = records.filter(record => record.id !== id);
+    await this.writeAll(filteredRecords);
+  }
+
+  async update(id, attrs) {
+    const records = await this.getAll();
+    const record = records.find(record => record.id === id);
+
+    if (!record) {
+      throw new Error(`Record with id ${id} not found`);
+    }
+
+    Object.assign(record, attrs);
+    await this.writeAll(records);
+  }
+
+  async getOneBy(filters) {
+    const records = await this.getAll();
+
+    for (let record of records) {
+      let found = true;
+
+      for (let key in filters) {
+        if (record[key] !== filters[key]) {
+          found = false;
+        }
+      }
+
+      if (found) {
+        return record;
+      }
+    }
   }
 }
 
-
-const test = async () => {
-const repo = new UsersRepository('users.json');
-await repo.create({email: "test@test.com", password: "hello"})
-const users = await repo.getAll()
-
-
-console.log(users)
-}
-
-test()
-
+module.exports = new UsersRepository('users.json');
