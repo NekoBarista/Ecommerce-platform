d3e11ead4a682d476e8500d7c5a3b81be278935f
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-08-01 14:23:23 +1000

    authentication process in place

--- repositories/users.js
+++ repositories/users.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const crypto = require('crypto');
-const util = require('util')
+const util = require('util');
 
-const scrypt = util.promisify(crypto.scrypt)
+const scrypt = util.promisify(crypto.scrypt);
+
 class UsersRepository {
   constructor(filename) {
     if (!filename) {
@@ -25,28 +26,31 @@ class UsersRepository {
     );
   }
 
-  async create(attributes) {
-    attributes.id = this.randomId();
-    const salt = crypto.randomBytes(8).toString('hex')
-   const buff = await scrypt(attributes.password, salt, 64)
+  async create(attrs) {
+    attrs.id = this.randomId();
+
+    const salt = crypto.randomBytes(8).toString('hex');
+    const buf = await scrypt(attrs.password, salt, 64);
 
     const records = await this.getAll();
-    const record = {...attributes, password: `${buff.toString('hex')}.${salt}`}
+    const record = {
+      ...attrs,
+      password: `${buf.toString('hex')}.${salt}`
+    };
     records.push(record);
 
     await this.writeAll(records);
 
-    return record
-
-
+    return record;
   }
 
-  async comparePassword(saved, supplied) {
-    const [hashed, salt] = saved.split('.')
-const hashedSuppliedBuff = await scrypt(supplied, salt, 64);
+  async comparePasswords(saved, supplied) {
+    // Saved -> password saved in our database. 'hashed.salt'
+    // Supplied -> password given to us by a user trying sign in
+    const [hashed, salt] = saved.split('.');
+    const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
 
-return hashed === hashedSuppliedBuff.toString('hex')
-
+    return hashed === hashedSuppliedBuf.toString('hex');
   }
 
   async writeAll(records) {
--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,61 +1,69 @@
+const express = require('express');
+const { check, validationResult } = require('express-validator');
 
-const express = require('express')
-const UsersRepository = require('../../repositories/users')
-const router = express.Router()
-const signupTemplate = require('../../views/admin/auth/signup')
-const signinTemplate = require('../../views/admin/auth/signin')
+const usersRepo = require('../../repositories/users');
+const signupTemplate = require('../../views/admin/auth/signup');
+const signinTemplate = require('../../views/admin/auth/signin');
+const {
+  requireEmail,
+  requirePassword,
+  requirePasswordConfirmation,
+  requireEmailExists,
+  requireValidPasswordForUser
+} = require('./validators');
+
+const router = express.Router();
 
 router.get('/signup', (req, res) => {
-    res.send(signupTemplate({ req }));
-  });
-  
-  router.post('/signup', async (req, res) => {
-    const { email, password, passwordConfirmation } = req.body;
-  const existingUser = await UsersRepository.getOneBy({ email });
-  if (existingUser) {
-     return  res.send("User already exists with this email") 
-  }
-  
-  if(password !== passwordConfirmation) {
-      return res.send("Password must match confirmation")
-  }
-  
-  const user = await UsersRepository.create({email, password})
-  
-  req.session.userId = user.id
-  
-    res.send('Account created!!!');
-  });
-  
-  
-  router.get('/signout', (req, res) => {
-    req.session = null;
-    res.send("You are now logged out")
-  
-  })
-  
-  router.get('/signin',(req, res) => {
-    res.send(signinTemplate())
-  })
-  
-  
-  router.post('/signin', async (req, res) => {
-    const { email, password } = req.body
-    const user = await UsersRepository.getOneBy({email})
-  
-    const validPassword = await UsersRepository.comparePassword(user.password, password)
-    if (!user) {
-      return res.send("No user found with this email")
-    }
-  
-    if (!validPassword) {
-      return res.send("Password incorrect, please try again")
-    }
-  
-    req.session.userId = user.id
-  
-    res.send("You are signed in!")
-    
-  })
+  res.send(signupTemplate({ req }));
+});
 
-  module.exports = router
+router.post(
+  '/signup',
+  [requireEmail, requirePassword, requirePasswordConfirmation],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.send(signupTemplate({ req, errors }));
+    }
+
+    const { email, password, passwordConfirmation } = req.body;
+    const user = await usersRepo.create({ email, password });
+
+    req.session.userId = user.id;
+
+    res.send('Account created!!!');
+  }
+);
+
+router.get('/signout', (req, res) => {
+  req.session = null;
+  res.send('You are logged out');
+});
+
+router.get('/signin', (req, res) => {
+  res.send(signinTemplate({}));
+});
+
+router.post(
+  '/signin',
+  [requireEmailExists, requireValidPasswordForUser],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.send(signinTemplate({ errors }));
+    }
+
+    const { email } = req.body;
+
+    const user = await usersRepo.getOneBy({ email });
+
+    req.session.userId = user.id;
+
+    res.send('You are signed in!!!');
+  }
+);
+
+module.exports = router;
