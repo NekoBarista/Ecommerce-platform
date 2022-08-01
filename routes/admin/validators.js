91d129cf9c8c5167bb2dccbd79fbec419ad1e633
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-08-01 18:23:00 +1000

    admin panel complete

--- routes/admin/validators.js
+++ routes/admin/validators.js
@@ -2,6 +2,15 @@ const { check } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 
 module.exports = {
+  requireTitle: check('title')
+    .trim()
+    .isLength({ min: 5, max: 40 })
+    .withMessage('Must be between 5 and 40 characters'),
+  requirePrice: check('price')
+    .trim()
+    .toFloat()
+    .isFloat({ min: 1 })
+    .withMessage('Must be a number greater than 1'),
   requireEmail: check('email')
     .trim()
     .normalizeEmail()
@@ -25,8 +34,6 @@ module.exports = {
       if (passwordConfirmation !== req.body.password) {
         throw new Error('Passwords must match');
       }
-
-      return true
     }),
   requireEmailExists: check('email')
     .trim()
