3e4c23906c8c5927a6fc85df50c3392733116ba1
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-08-01 15:18:38 +1000

    authentication styling complete

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
 
+
 const app = express();
+app.use(express.static('public'))
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
