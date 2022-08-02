2b99bc294bee3b0fad5fa3ac17ae4824f19ab3af
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-08-02 16:56:31 +1000

    bug fixes

--- routes/carts.js
+++ routes/carts.js
@@ -32,7 +32,7 @@ router.post('/cart/products', async (req, res) => {
     items: cart.items
   });
 
-  return res.redirect('/cart')
+ return res.redirect('/cart')
 });
 
 // Receive a GET request to show all items in cart
@@ -51,6 +51,21 @@ router.get('/cart', async (req, res) => {
 
   res.send(cartShowTemplate({ items: cart.items }));
 });
+
+
+router.post('/cart/products/delete', async(req, res) => {
+  const { itemId } = req.body
+  const cart = await cartsRepo.getOne(req.session.cartId);
+  const items = cart.items.filter(item => item.id !== itemId)
+
+  await cartsRepo.update(req.session.cartId, { items })
+
+  
+ return res.redirect('/cart')
+
+  
+
+})
 
 // Receive a post request to delete an item from a cart
 
