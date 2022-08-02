3e2d84e3ee9d73b86d4d1f4616f707270edd0101
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-08-02 14:30:39 +1000

    render product list complete

---
+++ routes/carts.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cartsRepo = require('../repositories/carts')
+const productsIndexTemplate = require('../views/products/index')
+
+const router = express.Router()
+
+
+router.get('/cart/products', async(req, res) => {
+
+    
+    })
+
+    router.post('/', async(req, res) => {
+    
+        
+        })
+
+        router.post('/cart/products/delete', async(req, res) => {
+
+    
+        })
+
+module.exports = router
