e0c0c234fbe5e7cd39c2b96efb92446cae718e47
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-08-02 17:12:36 +1000

    bug fixes

--- routes/carts.js
+++ routes/carts.js
@@ -5,29 +5,24 @@ const cartShowTemplate = require('../views/carts/show');
 
 const router = express.Router();
 
-// Receive a post request to add an item to a cart
 router.post('/cart/products', async (req, res) => {
-  // Figure out the cart!
   let cart;
   if (!req.session.cartId) {
-    // We dont have a cart, we need to create one,
-    // and store the cart id on the req.session.cartId
-    // property
+  
     cart = await cartsRepo.create({ items: [] });
     req.session.cartId = cart.id;
   } else {
-    // We have a cart! Lets get it from the repository
     cart = await cartsRepo.getOne(req.session.cartId);
   }
 
-  const existingItem = cart.items.find(item => item.id === req.body.productId);
-  if (existingItem) {
-    // increment quantity and save cart
-    existingItem.quantity++;
-  } else {
-    // add new product id to items array
-    cart.items.push({ id: req.body.productId, quantity: 1 });
-  }
+  // const existingItem = cart.items.find(item => item.id === req.body.productId);
+  // if (existingItem) {
+  //   // increment quantity and save cart
+  //   existingItem.quantity++;
+  // } else {
+  //   // add new product id to items array
+  //   cart.items.push({ id: req.body.productId, quantity: 1 });
+  // }
   await cartsRepo.update(cart.id, {
     items: cart.items
   });
@@ -35,7 +30,6 @@ router.post('/cart/products', async (req, res) => {
  return res.redirect('/cart')
 });
 
-// Receive a GET request to show all items in cart
 router.get('/cart', async (req, res) => {
   if (!req.session.cartId) {
     return res.redirect('/');
@@ -67,6 +61,5 @@ router.post('/cart/products/delete', async(req, res) => {
 
 })
 
-// Receive a post request to delete an item from a cart
 
 module.exports = router;
